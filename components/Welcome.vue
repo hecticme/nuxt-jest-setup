642805b24d5ed82cb20d9355bd7f22f10802d02b
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2024-06-20 09:54:37 +0700

    feat: create `Welcome` component

---
+++ components/Welcome.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2 class="welcome">
+    Hello! Welcome
+  </h2>
+</template>
+
+<style scoped>
+.welcome {
+  font-size: 2rem;
+  font-weight: 600;
+  letter-spacing: -0.025em;
+}
+</style>
