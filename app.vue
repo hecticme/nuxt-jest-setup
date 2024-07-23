d8b74d17c5c936766dda0736152efc8aef1713af
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2024-07-23 22:44:05 +0700

    style: update app.vue's layout

--- app.vue
+++ app.vue
@@ -1,7 +1,26 @@
 <template>
-  <div>
-    <Welcome />
+  <div
+    id="app"
+    class="app-container"
+  >
+    <TheHeader />
 
-    <FormSubmitter />
+    <main class="main-container">
+      <NuxtPage />
+    </main>
   </div>
 </template>
+
+<style scoped>
+.app-container {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.main-container {
+  flex: 1;
+  max-width: 80rem;
+  margin-inline: auto;
+}
+</style>
