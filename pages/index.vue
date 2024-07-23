767753778d7f1fbe3b03e2decadffeedad502649
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2024-07-23 22:38:18 +0700

    feat: create `pages/index`

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1 class="title">
+      This is the homepage
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+.title {
+  font-size: 2rem;
+  font-weight: 600;
+}
+</style>
