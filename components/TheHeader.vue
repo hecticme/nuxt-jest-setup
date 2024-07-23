8cdc55a4acd67b4551124d93aaea5bbb6cc66e3d
H. Ngọc Minh <hangoc.minh1306@gmail.com>
2024-07-23 22:37:12 +0700

    feat: create TheHeader component

---
+++ components/TheHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="header">
+    <div class="header-inner">
+      <NuxtLink to="/">
+        Home
+      </NuxtLink>
+
+      <NuxtLink to="/playground">
+        Playground
+      </NuxtLink>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  position: sticky;
+  top: 0;
+  padding-inline: 0.5rem;
+  padding-block: 1rem;
+}
+
+.header-inner {
+  max-width: 60rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
